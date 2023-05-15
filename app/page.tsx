cabaa39d98d03c338153bef022ccab461998a831
Brandon Trump <brandontrump101@gmail.com>
2023-05-15 16:25:14 +1200

    feat: completed hero section of template

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,31 @@
+import workingImg from "@/assets/images/illustration-working.svg";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>{/* <h1>Home Page</h1> */}</div>;
+  return (
+    <div className="pt-32">
+      <Image
+        src={workingImg}
+        alt="hero"
+        className="w-full h-full md:max-w-4xl md:mx-auto"
+      ></Image>
+
+      <div className="flex flex-col justify-center items-center text-center space-y-8 mt-14">
+        <div className="space-y-4">
+          <h1 className="font-bold text-4xl">More than just shorter links</h1>
+          <p className="text-gray-400 text-xl">
+            Build your brand&apos;s recognition and get detailed insights on how
+            your links are performing.
+          </p>
+        </div>
+        <Link
+          href={"#"}
+          className="bg-teal-400 py-5 px-14 rounded-full cursor-pointer text-white font-bold text-xl"
+        >
+          Get Started
+        </Link>
+      </div>
+    </div>
+  );
 }
