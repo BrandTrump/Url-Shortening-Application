80398e4482772dc0d3779ddd65ee8544c9818e5e
Brandon Trump <brandontrump101@gmail.com>
2023-05-20 01:52:51 +1200

    feat: implemented server action to fetch a shortened url based on the search input

--- components/ShorteningField.tsx
+++ components/ShorteningField.tsx
@@ -1,23 +1,24 @@
-import bgMobile from "@/assets/images/bg-shorten-mobile.svg";
+import ShortenButton from "./ShortenButton";
+import ShortenedUrlCard from "./ShortenedUrlCard";
 
 function ShorteningField() {
-  return (
-    <div className="bg-violet-950 w-96 mx-auto p-10 mt-20 rounded-xl">
-      <form action="" className="flex flex-col space-y-8">
-        <input
-          type="text"
-          className="rounded-md p-3"
-          placeholder="Shorten a link here..."
-        />
-        <button
-          type="submit"
-          className="bg-teal-400 text-white font-bold p-3 rounded-md hover:bg-teal-600"
-        >
-          Shorten It!
-        </button>
-      </form>
-    </div>
-  );
+  const shortenUrl = async (search: string) => {
+    "use server";
+
+    const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${search}`);
+
+    if (!res.ok) return undefined;
+
+    const shortenedUrl = await res.json();
+
+    const urlResponse = shortenedUrl.result.short_link;
+
+    console.log("Short URL: " + urlResponse);
+
+    return urlResponse;
+  };
+
+  return <ShortenedUrlCard search={shortenUrl} />;
 }
 
 export default ShorteningField;
