a95c12b24bc138c9411ca20e11cf35306895a297
Brandon Trump <brandontrump101@gmail.com>
2023-05-20 01:51:34 +1200

    style: change background color from white to light gray

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div>
+        <div className="bg-gray-100 h-screen">
           <Navbar />
           {children}
         </div>
