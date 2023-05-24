3e70cc23d3189567466304c06ea74b944f2c0b65
Brandon Trump <brandontrump101@gmail.com>
2023-05-24 14:24:20 +1200

    feat: included info section & cta component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import workingImg from "@/assets/images/illustration-working.svg";
+import CallToAction from "@/components/CallToAction";
+import InfoSection from "@/components/InfoSection";
 import ShorteningField from "@/components/ShorteningField";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,11 +8,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <div className="md:bg-white pb-20">
+      <div className="bg-white pb-40 md:pb-20">
         <div className="pt-36 flex flex-col-reverse md:justify-between md:flex-row md:pb-20 md:items-center overflow-hidden md:space-x-20">
-          <div className="flex flex-col justify-center items-center text-center space-y-8 mt-14 md:text-start md:items-start">
+          <div className="flex flex-col justify-center items-center text-center px-8 space-y-8 mt-14 md:text-start md:items-start md:px-8">
             <div className="space-y-4">
-              <h1 className="font-bold text-4xl md:text-6xl lg:text-7xl">
+              <h1 className="font-bold text-5xl md:text-6xl lg:text-7xl">
                 More than just shorter links
               </h1>
               <p className="text-gray-400 text-xl lg:text-2xl">
@@ -26,16 +28,18 @@ export default function Home() {
             </Link>
           </div>
 
-          <div className="overflow-hidden w-full md:max-w-4xl md:scale-110">
+          <div className="overflow-hidden w-full md:max-w-4xl md:scale-110 py-12">
             <Image
               src={workingImg}
               alt="hero"
-              className="w-full h-full"
+              className="w-full h-full translate-x-1/4 scale-125 md:scale-110 md:translate-x-20"
             ></Image>
           </div>
         </div>
       </div>
       <ShorteningField />
+      <InfoSection />
+      <CallToAction />
     </div>
   );
 }
