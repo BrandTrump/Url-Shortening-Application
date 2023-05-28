4f9c005c72299a33294c9935f56bb8f31622de57
Brandon Trump <brandontrump101@gmail.com>
2023-05-28 14:44:36 +1200

    style: fixed mobile responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -52,7 +52,7 @@ function Navbar() {
               viewBox="0 0 24 24"
               strokeWidth="1.5"
               stroke="currentColor"
-              className="w-6 h-6 cursor-pointer"
+              className="w-8 h-8 cursor-pointer"
               onClick={() => setToggle(true)}
             >
               <path
@@ -62,50 +62,49 @@ function Navbar() {
               />
             </svg>
           ) : (
-            <div className="absolute top-20 left-1/2 -translate-x-1/2 z-50 border bg-violet-950 text-white font-bold rounded-2xl p-4 w-5/6">
-              <div className="flex flex-col items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6 cursor-pointer"
-                  onClick={() => setToggle(false)}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-8 h-8 cursor-pointer"
+                onClick={() => setToggle(false)}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+              <div className="absolute top-24 left-1/2 -translate-x-1/2 z-50 border bg-violet-950 text-white font-bold rounded-2xl p-4 w-5/6">
+                <div className="flex flex-col items-center">
+                  <div className="flex flex-col justify-between items-center text-center space-y-8 p-6 divide-y divide-white/30 w-full">
+                    <ul className="mt-5 space-y-4">
+                      <li className="">
+                        <Link href={"#"}>Features</Link>
+                      </li>
+                      <li className="">
+                        <Link href={"#"}>Pricing</Link>
+                      </li>
+                      <li className="">
+                        <Link href={"#"}>Resources</Link>
+                      </li>
+                    </ul>
 
-                <div className="flex flex-col justify-between items-center text-center space-y-8 p-6">
-                  <ul className="mt-5 space-y-4">
-                    <li className="">
-                      <Link href={"#"}>Features</Link>
-                    </li>
-                    <li className="">
-                      <Link href={"#"}>Pricing</Link>
-                    </li>
-                    <li className="">
-                      <Link href={"#"}>Resources</Link>
-                    </li>
-                  </ul>
-
-                  <br className="h-1" />
-
-                  <ul className="space-y-4">
-                    <li className="">
-                      <Link href={"#"}>Login</Link>
-                    </li>
-                    <li className="bg-teal-400 text-white px-8 md:py-2 rounded-full w-full py-3 cursor-pointer">
-                      <Link href={"#"}>Sign Up</Link>
-                    </li>
-                  </ul>
+                    <ul className="space-y-4 w-full pt-6">
+                      <li className="">
+                        <Link href={"#"}>Login</Link>
+                      </li>
+                      <li className="bg-teal-400 text-white md:py-2 rounded-full w-full py-3 cursor-pointer">
+                        <Link href={"#"}>Sign Up</Link>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
+            </>
           )}
         </div>
       </div>
