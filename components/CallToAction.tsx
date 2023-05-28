9f5b598e2435d61d1d3c7d77c88c4703d2ec5edf
Brandon Trump <brandontrump101@gmail.com>
2023-05-28 14:45:37 +1200

    style: changed background to match with the template image

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -2,15 +2,17 @@ import Link from "next/link";
 
 function CallToAction() {
   return (
-    <div className="bg-violet-950 w-full">
-      <div className="py-20 px-10 text-center text-white flex flex-col space-y-4">
-        <h1 className="font-bold text-3xl">Boost your links today</h1>
-        <Link
-          href="#"
-          className="bg-teal-500 py-3 px-10 w-auto mx-auto rounded-full hover:bg-teal-300 font-bold"
-        >
-          Get Started
-        </Link>
+    <div className="bg-violet-950">
+      <div className="bg-[url('/bg-boost-desktop.svg')] object-cover w-full h-full bg-no-repeat">
+        <div className="py-16 px-10 text-center text-white flex flex-col space-y-4">
+          <h1 className="font-bold text-3xl">Boost your links today</h1>
+          <Link
+            href="#"
+            className="bg-teal-500 py-3 px-10 w-auto mx-auto rounded-full hover:bg-teal-300 font-bold"
+          >
+            Get Started
+          </Link>
+        </div>
       </div>
     </div>
   );
