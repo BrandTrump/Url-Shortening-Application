e30b4bd575e8e46021e610042cc1e7653f21c6a4
Brandon Trump <brandontrump101@gmail.com>
2023-05-30 23:43:08 +1200

    style: improved responsiveness

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -4,7 +4,7 @@ function CallToAction() {
   return (
     <div className="bg-violet-950">
       <div className="bg-[url('/bg-boost-desktop.svg')] object-cover w-full h-full bg-no-repeat">
-        <div className="py-16 px-10 text-center text-white flex flex-col space-y-4">
+        <div className="py-14 px-10 text-center text-white flex flex-col space-y-4 md:py-18">
           <h1 className="font-bold text-3xl">Boost your links today</h1>
           <Link
             href="#"
