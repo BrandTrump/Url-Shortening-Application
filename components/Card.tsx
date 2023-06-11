110f80ed4ef49f530d269a80a27a27da4696eb5e
Brandon Trump <brandontrump101@gmail.com>
2023-06-12 00:24:59 +1200

    feat: added an onClick function to copy the displayed link

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,10 +1,30 @@
+"use client";
+
+import { Dispatch, SetStateAction, useState } from "react";
+
 type Props = {
   shortenedUrl: string;
   searchString: string;
+  toggleCopy: boolean;
+  setToggleCopy: Dispatch<SetStateAction<boolean>>;
 };
 
-function Card({ shortenedUrl, searchString }: Props) {
-  console.log(shortenedUrl);
+function Card({
+  shortenedUrl,
+  searchString,
+  toggleCopy,
+  setToggleCopy,
+}: Props) {
+  const handleCopyLink = () => {
+    navigator.clipboard
+      .writeText(shortenedUrl)
+      .then(() => console.log(shortenedUrl))
+      .catch((error) => console.error(error))
+      .finally(() => {
+        setToggleCopy(true);
+      });
+  };
+
   return (
     <div className="bg-white w-5/6 flex justify-center flex-col items-center mx-auto mt-10 rounded-xl space-y-4 p-6 md:w-3/4 md:flex-row md:items-center md:space-y-0 md:space-x-10 shadow-sm">
       <div className="flex flex-col space-y-2 w-full font-bold text-xl md:flex-row md:items-center md:space-y-0 md:justify-between md:space-x-0 md:flex-wrap">
@@ -17,8 +37,15 @@ function Card({ shortenedUrl, searchString }: Props) {
           https://{shortenedUrl}
         </a>
       </div>
-      <button className="bg-teal-400 text-white font-bold p-3 rounded-md hover:bg-teal-200 disabled:bg-teal-800 w-full md:w-auto md:px-10">
-        Copy
+      <button
+        onClick={handleCopyLink}
+        className={
+          !toggleCopy
+            ? "bg-teal-400 text-white font-bold p-3 rounded-md hover:bg-teal-200 disabled:bg-teal-800 w-full md:w-auto md:px-10"
+            : "bg-violet-900 text-white font-bold p-3 rounded-md w-full md:w-auto md:px-10"
+        }
+      >
+        {!toggleCopy ? "Copy" : "Copied!"}
       </button>
     </div>
   );
